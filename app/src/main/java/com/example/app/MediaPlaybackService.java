7e71cd3a22241813dfa0f0bbb3fa526b8917327d
purplescorpion1 <147272307+purplescorpion1@users.noreply.github.com>
2025-08-10 15:26:48 +0100

    Include all correct imports

--- app/src/main/java/com/example/app/MediaPlaybackService.java
+++ app/src/main/java/com/example/app/MediaPlaybackService.java
@@ -13,8 +13,13 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+
 import androidx.core.app.NotificationCompat;
 import androidx.media.app.NotificationCompat.MediaStyle;
 import org.json.JSONObject;
