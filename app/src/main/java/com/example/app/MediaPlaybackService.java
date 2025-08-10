6284c5ee6f9056c3a286779ecc50f6944085fd74
purplescorpion1 <147272307+purplescorpion1@users.noreply.github.com>
2025-08-10 13:52:51 +0100

    Add in correct inports

--- app/src/main/java/com/example/app/MediaPlaybackService.java
+++ app/src/main/java/com/example/app/MediaPlaybackService.java
@@ -14,7 +14,10 @@ import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+
 import androidx.core.app.NotificationCompat;
 import androidx.media.app.NotificationCompat.MediaStyle;
 
